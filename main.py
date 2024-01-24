b1d7fd08e1bf3f7bb160a056cab29524c2bfd8dc
blandineb38 <blandine.bouton64@gmail.com>
2024-01-24 14:41:40 +0100

    Sorciere

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 class Main:
-    def __init__(self):
-        self.__joueur_mort_nuit = []
+    def __init__(self, nom_joueurs: list):
+        self.__joueur_mort = []
+        self.__sauve = True
+        self.__poison = True
 
-    def get_joueur_mort_nuit(self):
-        return self.__joueur_mort_nuit
+    def get_joueur_mort(self):
+        return self.__joueur_mort
+    def sorciere(self, choix : str): #un bouton sauve avec une fonction qui verifie que sauve est true et un bouton qui affiche nom_joueur mort et Yes
+        if choix == "Sauve":
+            self.__sauve = False
+            print("Le joueur mort est : " + self.__joueur_mort[-1])
+            del(self.__joueur_mort[-1])
+        if choix == "Poison":
+            self.__poison = False
+            vote = "nom_du_joueur"
+            self.__joueur_mort.append(vote)
 
-    def set_joueur_mort_nuit(self, new_joueur : list) -> None:
-        self.__joueur_mort_nuit = new_joueur
--- src/Sorciere.py
+++ src/Sorciere.py
@@ -1,7 +1,6 @@
 from main import Main
 
 class sorciere:
-<<<<<<< HEAD
     def __init__(self, choix : list):
         self.__joueur_mort_nuit = Main().get_joueur_mort_nuit() #nom du joueur
         self.__choix = choix[0]
@@ -11,16 +10,5 @@ class sorciere:
         if self.__choix == self.__biblio[0]:
             print("Sorciere a décidé de : " + self.__choix)
 
-
-=======
-    def __init__(self, choix):
-        self.joueur_mort_nuit = Main().get_joueur_mort_nuit() #nom du joueur
-        self.choix = choix
-        self.dico = {"Vie":1, "Mort":-1, "Neutre":0}
-        
-    def choix(self):
-        pass
-        
->>>>>>> origin/blandine
 
 
