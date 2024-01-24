400435b2d0f34ff3049d12b5dccb262643004cab
Thomas <thomas.royer24@gmail.com>
2024-01-24 14:17:39 +0100

    class player

--- src/Loup.py
+++
@@ -1,6 +0,0 @@
-
-class Loup:
-
-    def __init__(self,name):
-        self.name = name
-        self.is_alive = 1
---
+++ src/Player.py
@@ -0,0 +1,22 @@
+
+class Player:
+
+    def __init__(self, name: str, role: str):
+        self.__name = name
+        self.__is_alive = 1
+        self.__role = role
+
+    def get_is_alive(self) -> int:
+        return self.__is_alive
+
+    def set_is_alive(self, value: int) -> None:
+        self.__is_alive = value
+
+    def get_role(self) -> str:
+        return self.__role
+
+    def set_role(self, value: str) -> None:
+        self.__role = value
+
+    def vote(self):
+        return input('Enter the name of the person you want to kill :')
