24359ef5a8d9af52e327f5d6971732c1ba225f32
blandineb38 <blandine.bouton64@gmail.com>
2024-01-24 17:09:11 +0100

    fonction sorciere / fonction creategame

---
+++ src/Player.py
@@ -0,0 +1,24 @@
+
+class Player:
+
+    def __init__(self, name: str, role: str):
+        self.__name = name
+        self.__is_alive = 1
+        self.__role = role
+
+    def get_name(self) -> str:
+        return self.__name
+    def get_is_alive(self) -> int:
+        return self.__is_alive
+
+    def set_is_alive(self, value: int) -> None:
+        self.__is_alive = value
+
+    def get_role(self) -> str:
+        return self.__role
+
+    def set_role(self, value: str) -> None:
+        self.__role = value
+
+    def vote(self):
+        return input()
