0ba5c5fa2e01550425ffc73f12b0630337471268
Thomas <thomas.royer24@gmail.com>
2024-01-25 16:35:31 +0100

    modification de la fonction vote

--- src/Player.py
+++ src/Player.py
@@ -1,4 +1,3 @@
-
 class Player:
 
     def __init__(self, name: str, role: str):
@@ -9,6 +8,9 @@ class Player:
     def get_is_alive(self) -> int:
         return self.__is_alive
 
+    def get_name(self) -> str:
+        return self.__name
+
     def set_is_alive(self, value: int) -> None:
         self.__is_alive = value
 
@@ -18,5 +20,10 @@ class Player:
     def set_role(self, value: str) -> None:
         self.__role = value
 
-    def vote(self):
-        return input()
+    def vote(self, all_Player) -> Player:
+        while 1:
+            name = input()
+            for players in all_Player:
+                if players.get_name() == name and players.get_is_alive() == 1:
+                    return players
+            print("Ce joueur est inexistant ou déjà mort, recommencer")
