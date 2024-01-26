d0932bc4f2abd968dd78d2a91ee8fff475c632e4
blandineb38 <blandine.bouton64@gmail.com>
2024-01-26 15:08:54 +0100

    fonction sorciere / fonction creategame

--- src/Player.py
+++ src/Player.py
@@ -1,4 +1,3 @@
-
 class Player:
 
     def __init__(self, name: str, role: str):
@@ -6,10 +5,11 @@ class Player:
         self.__is_alive = 1
         self.__role = role
 
-    def get_name(self) -> str:
-        return self.__name
     def get_is_alive(self) -> int:
         return self.__is_alive
+
+    def get_name(self) -> str:
+        return self.__name
 
     def set_is_alive(self, value: int) -> None:
         self.__is_alive = value
@@ -20,5 +20,10 @@ class Player:
     def set_role(self, value: str) -> None:
         self.__role = value
 
-    def vote(self):
-        return input()
+    def vote(self, all_Player):
+        while 1:
+            name = input()
+            for players in all_Player:
+                if players.get_name() == name and players.get_is_alive() == 1:
+                    return players
+            print("Ce joueur est inexistant ou déjà mort, recommencer")
