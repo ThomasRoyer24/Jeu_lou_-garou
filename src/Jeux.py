3ae50ae4a91fbdb5c03140484bfe23e39b6cb693
blandineb38 <blandine.bouton64@gmail.com>
2024-02-05 22:21:12 +0100

    MAJ fichier master_ip

--- src/Jeux.py
+++ src/Jeux.py
@@ -149,7 +149,7 @@ class Jeux:
             if key == nb_total:
                 self.__loups_villageois.append(DICO_NBJOUEURS[key][0])
                 self.__loups_villageois.append(nb_player-DICO_NBJOUEURS[key][0])
-
+                print(self.__loups_villageois)
                 for i in range(nb_player):
                     #creation des noms des joueurs
                     liste_player.append("p"+str(i))
@@ -365,7 +365,7 @@ class Jeux:
                 villageois_mort +=1
             if players.get_role() == 'loups' and players.get_is_alive() == 0:
                 loups_mort +=1
-
+        print(self.__loups_villageois)
         if loups_mort == self.__loups_villageois[0]:
             self.__server.broadcast_message("\n Meneur <--> Les villageois ont gagné Youpiii")
             print("Les villageois ont gagné")
